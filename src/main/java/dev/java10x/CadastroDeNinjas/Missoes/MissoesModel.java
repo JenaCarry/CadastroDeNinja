edaac22c3e42158bb6c792be56af726b1b2adc6e
JenaCarry <jeandaisu@gmail.com>
2026-05-07 20:49:09 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -8,9 +8,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
   @Id
@@ -20,46 +26,4 @@ public class MissoesModel {
   private String dificuldade;
   @OneToMany(mappedBy = "missoes")
   private List<NinjaModel> ninjas;
-
-  public MissoesModel() {
-  }
-
-  public MissoesModel(Long id, String nome, String dificuldade, List<NinjaModel> ninja) {
-    this.id = id;
-    this.nome = nome;
-    this.dificuldade = dificuldade;
-    this.ninjas = ninja;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getNome() {
-    return nome;
-  }
-
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
-
-  public String getDificuldade() {
-    return dificuldade;
-  }
-
-  public void setDificuldade(String dificuldade) {
-    this.dificuldade = dificuldade;
-  }
-
-  public List<NinjaModel> getNinjas() {
-    return ninjas;
-  }
-
-  public void setNinjas(List<NinjaModel> ninjas) {
-    this.ninjas = ninjas;
-  }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -9,9 +9,15 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
   @Id
@@ -25,36 +31,4 @@ public class NinjaModel {
   @JoinColumn(name = "missoes_id") // FK
   private MissoesModel missoes;
 
-  public NinjaModel() {
-  }
-
-  public NinjaModel(String nome, String email, int idade) {
-    this.nome = nome;
-    this.email = email;
-    this.idade = idade;
-  }
-
-  public String getNome() {
-    return nome;
-  }
-
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public int getIdade() {
-    return idade;
-  }
-
-  public void setIdade(int idade) {
-    this.idade = idade;
-  }
 }
